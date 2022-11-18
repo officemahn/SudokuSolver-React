eb7d3e03da5177ba0ae145f0324f92af31e5014a
David Oladipo <David.Oladipo@outlook.com>
2022-11-18 10:25:35 -0700

    Landing page and css styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import logo2 from './images/sudoku.gif'
 
 function App() {
+  useEffect(() => {
+    document.title = "Sudoku Game";  
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <section>
+        <header>
+          <img src="./images/logo.png" width="100" alt="logo" />
+          <div>
+            <ul>
+              <li>HOME</li>
+              <li>RULES</li>
+              <li>ABOUT</li>
+
+            </ul>
+          </div>
+        </header>
+      </section>
+
+      <section id="main">
+        <div className="main-text">
+          <h1 className="title">Sudoku Twist</h1>
+          <p className="subtitle"> Do you think you have what it takes to be on the hall of fame?</p > 
+          <input type="text" id="userName" placeholder="Type in your name" />
+          <button> Game On</button>
+        </div>
+
+        <img src={logo2} width ="500" alt="sudoku-main"/>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
