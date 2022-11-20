8fd96181253132e2701ea7654330ea01cfae1d7b
Emeka Umeozo <emeka.umeozo@navblue.aero>
2022-11-19 20:30:41 -0500

    wip

--- src/components/boardComponent.jsx
+++ src/components/boardComponent.jsx
@@ -12,16 +12,15 @@ class Board extends Component {
     }
 
     componentDidUpdate(){
-        console.log("Did i rerender?")
-        console.log(this.state.board)
     }
+
     handleCellClick = (cellRow, cellCol) => {
 
         console.log(cellRow, cellCol)
         const highlightedCell = {...this.state.highlightedCell};
         highlightedCell["row"] = cellRow;
         highlightedCell['col'] = cellCol;
-        this.setState({highlightedCell});
+        this.setState({highlightedCell: highlightedCell});
     }
 
     updateHighlightedCell = (cellRow, cellCol, value) => {
@@ -31,63 +30,58 @@ class Board extends Component {
             return;
         }
 
-        const updated_board = this.state.board.slice();
+        const updated_board = [...this.state.board];
         updated_board[cellRow][cellCol] = value;
-        this.setState({board: updated_board})
-        console.log(this.state.board)
+        this.setState({board: updated_board});
+        console.log(this.state.board);
     }
 
     getCell(row){
-
-        var cells = []
+        var cells = [];
         for(let col = 0; col < 9; col++){
             cells.push(<Cell onCellClick={this.handleCellClick} row={row} col={col} key={`${row}-${col}`} board={this.state.board}/>);
         }
 
-        return cells
+        return cells;
     }
 
-    board_ui = 
-    <div>
-        <div>{ this.state.highlightedCell.row == undefined? "" : this.state.board[this.state.highlightedCell.row][this.state.highlightedCell.col]}</div>
-        <div>
-            <table border="2" frame="hsides" rules="groups"
-                    summary="sudoku board">
-            <colgroup align="center" span="3"/>
-            <colgroup align="center" span="3"/>
-            <colgroup align="center" span="3"/>
-            <tbody>
-                <tr>{this.getCell(0)}</tr>
-                <tr>{this.getCell(1)}</tr>
-                <tr>{this.getCell(2)}</tr>  
-            </tbody>
-            <tbody>
-                <tr>{this.getCell(3)}</tr>
-                <tr>{this.getCell(4)}</tr>
-                <tr>{this.getCell(5)}</tr>              
-            </tbody>     
-            <tbody>
-                <tr>{this.getCell(6)}</tr>
-                <tr>{this.getCell(7)}</tr>
-                <tr>{this.getCell(8)}</tr>              
-            </tbody>
-            </table>
+    render() {
+        return <div>
+                <div>
+                    <table border="2" frame="hsides" rules="groups"
+                            summary="sudoku board">
+                    <colgroup align="center" span="3"/>
+                    <colgroup align="center" span="3"/>
+                    <colgroup align="center" span="3"/>
+                    <tbody>
+                        <tr>{this.getCell(0)}</tr>
+                        <tr>{this.getCell(1)}</tr>
+                        <tr>{this.getCell(2)}</tr>  
+                    </tbody>
+                    <tbody>
+                        <tr>{this.getCell(3)}</tr>
+                        <tr>{this.getCell(4)}</tr>
+                        <tr>{this.getCell(5)}</tr>              
+                    </tbody>     
+                    <tbody>
+                        <tr>{this.getCell(6)}</tr>
+                        <tr>{this.getCell(7)}</tr>
+                        <tr>{this.getCell(8)}</tr>              
+                    </tbody>
+                    </table>
+                </div>
+                <div>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 1)}>1</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 2)}>2</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 3)}>3</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 4)}>4</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 5)}>5</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 6)}>6</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 7)}>7</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 8)}>8</button>
+                    <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 9)}>9</button>
+                </div>
         </div>
-        <div>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 1)}>1</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 2)}>2</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 3)}>3</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 4)}>4</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 5)}>5</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 6)}>6</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 7)}>7</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 8)}>8</button>
-            <button onClick={() => this.updateHighlightedCell(this.state.highlightedCell.row, this.state.highlightedCell.col, 9)}>9</button>
-        </div>
-    </div>
-
-    render() { 
-        return this.board_ui;
     }
 }
 
